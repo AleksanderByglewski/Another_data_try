566299ca7dc3ace1a0cc1ffe3fc4836d9deb5a4b
AleksanderByglewski <67806043+AleksanderByglewski@users.noreply.github.com>
2020-09-28 19:29:24 +0200

    Adding the class updates with Kiss principle

---
+++ File/venv/Tag_class/Tag_element.py
@@ -0,0 +1,11 @@
+class Element():
+    def __init__(self, x,y,z):
+        z=[]
+        if z is not None:
+            self.Tags=z
+        y=[]
+        if y is not None:
+            self.Data=y
+
+        self.FULL_IDENTIFIER_DATA=x
+        #Apply the Kiss principle, okay big guy?
