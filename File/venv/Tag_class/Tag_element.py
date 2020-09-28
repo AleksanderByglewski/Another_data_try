7ef71c711500043e46d4b7cb8b72c5180d6384fa
AleksanderByglewski <67806043+AleksanderByglewski@users.noreply.github.com>
2020-09-28 22:50:14 +0200

    Progress of class

--- File/venv/Tag_class/Tag_element.py
+++ File/venv/Tag_class/Tag_element.py
@@ -1,11 +1,28 @@
-class Element():
-    def __init__(self, x,y,z):
-        z=[]
-        if z is not None:
-            self.Tags=z
-        y=[]
-        if y is not None:
-            self.Data=y
 
-        self.FULL_IDENTIFIER_DATA=x
+import pickle
+
+class Element():
+    def __init__(self, name,tag_list,email_list):
+        self.name=name
+        email_list=[]
+        tag_list=[]
+        if tag_list is not None:
+            self.tag_list=tag_list
+        if email_list is not None:
+            self.tag_list = email_list
+
         #Apply the Kiss principle, okay big guy?
+
+    def __repr__(self):
+        expr=self.x
+        expr+=' ['
+        for tag in self.tag_list:
+            expr+=tag
+        expr+='] ['
+        for email_list in self.email.list:
+            expr+=email_list
+        expr+=']'
+        return(expr)
+class Tag_Element():
+    def __init__(self,x,y):
+        super.__init__(x,y,z='')
