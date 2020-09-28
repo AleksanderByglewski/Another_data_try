09523d36514c1145ac095eac07648f36008cd113
AleksanderByglewski <67806043+AleksanderByglewski@users.noreply.github.com>
2020-09-28 23:07:53 +0200

    Finishing the class, all that is left is the flatten method

--- File/venv/Tag_class/Tag_element.py
+++ File/venv/Tag_class/Tag_element.py
@@ -1,11 +1,11 @@
 
-import pickle
-
 class Element():
-    def __init__(self, name,tag_list,email_list):
+    def __init__(self, name='',tag_list=None,email_list=None):
+        specializer()
+    def specializer(self, name='',tag_list=None,email_list=None):
         self.name=name
-        email_list=[]
         tag_list=[]
+        email_list=[]
         if tag_list is not None:
             self.tag_list=tag_list
         if email_list is not None:
@@ -23,6 +23,18 @@ class Element():
             expr+=email_list
         expr+=']'
         return(expr)
+    def join(self,partner):
+        self.tag_list.extend(partner.tag_list)
+        self.email_list.extend(partner.email_list)
+    def read_in(line):
+        list_line=line.split(' ')
+        name_to_pass=list_line[0]
+        tag_list=(list_line[1][1:-1]).split(' ')
+        email_list=(list_line[2][1:-1]).split(' ')
+        dead_friend=Element(name.x,tag_list,email_list)
+        self.join(dead_friend)
+
+
 class Tag_Element():
     def __init__(self,x,y):
-        super.__init__(x,y,z='')
+        super.__init__(x,y,z=None)
